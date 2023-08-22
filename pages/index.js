58181301ac5a6ec51009e9c112382b2bfb1bc94a
lauvmepls <jdpsunga.student@ua.edu.ph>
2023-08-23 01:15:03 +0800

    changes

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,14 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, my Name is Jhon Denielle Sunga a 3rd year BS Computer Engineering student at University of the Assumption</p>
-        
+        <h1><b>My impression of the first month as a CPE third year student.</b></h1>
+        <p>Being a Computer Engineering student is really fun, challenging but fun.</p>
+        <p>Our schedule is kinda hectic and is very tiring sometimes.</p>
+        <p>Our professor in Software Development is very kind and very handsome and he gives very very high grades.</p>
+        <p>I hope he lives a fulfilling life and a healthy one.</p>
+        <p>I hope I won't fail on all of my subjects so I can graduate, yehey!</p>
+        <p>We are trying to build a sample website using:{' '}
+        <a href="https://nextjs.org/learn"> our Next.js tutorial </a> </p> 
       </section>
     </Layout>
   );
