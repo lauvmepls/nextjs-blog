a56e150ad17a676c8803256d19c8122d15b25e48
lauvmepls <jdpsunga.student@ua.edu.ph>
2023-09-14 16:41:36 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -9,16 +9,21 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <h1><b>My impression of the first month as a CPE third year student.</b></h1>
-        <p>Being a Computer Engineering student is really fun, challenging but fun.</p>
-        <p>Our schedule is kinda hectic and is very tiring sometimes.</p>
-        <p>Our professor in Software Development is very kind and very handsome and he gives very very high grades.</p>
-        <p>I hope he lives a fulfilling life and a healthy one.</p>
-        <p>I hope I won't fail on all of my subjects so I can graduate, yehey!</p>
-        <p>I know now how to push update my website</p>
-        <p>We are trying to build a sample website using:{' '}
-        <a href="https://nextjs.org/learn"> our Next.js tutorial </a> </p> 
+        <p>10 Things That Require Zero Talent</p>
+        <p>• 1.Being On Time </p>
+        <p>• 2. Making An Effort</p>
+        <p>• 3. Being High Energy</p>
+        <p>• 4. Having A Positive Attitude</p>
+        <p>• 5. Being Passionate</p>
+        <p>• 6. Using Good Body Language</p>
+        <p>• 7. Being Coachable</p>
+        <p>• 8. Doing A Little Extra</p>
+        <p>• 9. Being Prepared</p>
+        <p>• 10. Having A Strong Work Ethic</p>
+        <p>• (This is a sample website - you’ll be building a site like this on{' '}
+        <a href="https://nextjs.org/learn"> our Next.js tutorial) </a> </p>
       </section>
     </Layout>
+
   );
 }
